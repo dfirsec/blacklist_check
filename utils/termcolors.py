472d3693380b013f318b9e96221d2131d5d71546
pulsecode <12067255+dfirsec@users.noreply.github.com>
2020-07-09 10:28:34 -0400

    Update termcolors.py

--- utils/termcolors.py
+++ utils/termcolors.py
@@ -14,7 +14,7 @@ class Termcolor:
     MAGENTA = Fore.MAGENTA
     GRAY = Fore.LIGHTBLACK_EX
     YELLOW = Fore.YELLOW
-    RED = Fore.RED
+    RED = Fore.LIGHTRED_EX
     RESET = Style.RESET_ALL
     PROCESSING = CYAN + "\u279C " + RESET
     SUCCESS = GREEN + "\u2714 " + RESET
@@ -22,9 +22,9 @@ class Termcolor:
     ERROR = RED + "\u2718" + RESET
     QUESTION = YELLOW + "\uFF1F" + RESET
     DOWNLOAD_ERR = f"{YELLOW} [DOWNLOAD ERROR]{RESET}"
-    SCANNER = f"{WARNING} {BOLD}{'SCANNER':10} {RESET}"
-    BLACKLISTED = f"{ERROR} {BOLD}{'BLACKLISTED':10} {RESET}"
-    CLEAN = f"{SUCCESS} {GRAY}{'NOT LISTED':10} "
+    SCANNER = f"{WARNING} {BOLD}{'SCANNER':2} {RESET}"
+    BLACKLISTED = f"{RED}\u2716{RESET}  Blacklisted >"
+    CLEAN = f"{SUCCESS} {GRAY}{'NOT LISTED':2} "
     DOTSEP = f"{GRAY}{'.' * 30}{RESET}"
     MISSING = f"\n{WARNING} Blacklist file missing -- use the '-u' option to download.\n{YELLOW}  Run: python blacklist_downloader.py -u{RESET}"
     OUTDATED = f"{WARNING}{YELLOW} Blacklist file is older than 1 day - - use the '-u' option to update.\n{RESET}"
